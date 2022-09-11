b124029389bd246818868b853e7fb0584e539e9f
Ihor Kharchenko <ihor.stavr@gmail.com>
2022-09-11 23:16:24 +0300

    refactoring phonebook done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,77 +1,71 @@
-import React, { Component } from 'react';
+import { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { ContactsList } from './PhoneBook/ContactsList';
 import { Filter } from './PhoneBook/Filter';
 import { PhoneBook } from './PhoneBook/AddContact';
 import { Box } from 'components/Box';
-export class App extends Component {
-  static propTypes = {
-    contacts: PropTypes.arrayOf(
-      PropTypes.shape({
-        id: PropTypes.string.isRequired,
-        name: PropTypes.string.isRequired,
-        number: PropTypes.string.isRequired,
-      })
-    ),
-    filter: PropTypes.string,
-  };
-  state = {
-    contacts: [
-      // { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      // { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      // { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      // { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
-    filter: '',
-  };
-  componentDidMount() {
-    const contacts = JSON.parse(localStorage.getItem('contacts'));
-    // const parsedContacts = JSON.parse(contacts);
-    if (contacts) {
-      this.setState({ contacts: contacts });
-    }
-  }
-  // console.log(parsedContacts);
+import { toast, ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
-  componentDidUpdate(prevProps, prevState) {
-    if (this.state.contacts !== prevState.contacts) {
-      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
-    }
-  }
-  formSubmitHandler = data => {
-    this.state.contacts.some(contact => contact.name === data.name)
-      ? alert(`${data.name} is alredy in your contacts`)
-      : this.setState(prevState => ({
-          contacts: [data, ...prevState.contacts],
-        }));
+const useLocalStorage = (key, defaultValue) => {
+  const [state, setState] = useState(() => {
+    return JSON.parse(window.localStorage.getItem(key)) ?? defaultValue;
+  });
+
+  useEffect(() => {
+    window.localStorage.setItem(key, JSON.stringify(state));
+  }, [key, state]);
+
+  return [state, setState];
+};
+
+export const App = () => {
+  const [contacts, setContacts] = useLocalStorage('contacts', []);
+  const [filter, setFilter] = useState('');
+
+  const formSubmitHandler = data => {
+    contacts.some(contact => contact.name === data.name)
+      ? toast.warn(`${data.name} is alredy in your contacts`)
+      : setContacts(prevState => [data, ...prevState]);
   };
-  changeFilter = event => {
-    this.setState({ filter: event.currentTarget.value });
-  };
-  getFilteredContacts = () => {
-    const normalizedFilter = this.state.filter.toLowerCase();
-    return this.state.contacts.filter(contact =>
+
+  const getFilteredContacts = () => {
+    const normalizedFilter = filter.toLowerCase();
+    return contacts.filter(contact =>
       contact.name.toLowerCase().includes(normalizedFilter)
     );
   };
-  deleteContact = contactId => {
-    this.setState(prevState => ({
-      contacts: prevState.contacts.filter(contact => contact.id !== contactId),
-    }));
+
+  const deleteContact = contactId => {
+    setContacts(prevState => [
+      ...prevState.filter(contact => contact.id !== contactId),
+    ]);
   };
 
-  render() {
-    const filteredContacts = this.getFilteredContacts();
-    return (
-      <Box as="main" p="4" width="620px" ml="auto" mr="auto" bg="secondaryText">
-        <h1>Phonebook</h1>
-        <PhoneBook onSubmit={this.formSubmitHandler} />
-        <Filter filter={this.state.filter} onChange={this.changeFilter} />
-        <ContactsList
-          contacts={filteredContacts}
-          onDeleteContact={this.deleteContact}
-        />
-      </Box>
-    );
-  }
-}
+  return (
+    <Box as="main" p="4" width="620px" ml="auto" mr="auto" bg="secondaryText">
+      <h1>Phonebook</h1>
+      <PhoneBook onSubmit={formSubmitHandler} />
+      <Filter
+        filter={filter}
+        onChange={event => setFilter(event.currentTarget.value)}
+      />
+      <ContactsList
+        contacts={getFilteredContacts()}
+        onDeleteContact={deleteContact}
+      />
+      <ToastContainer position="top-center" autoClose={3000} />
+    </Box>
+  );
+};
+
+App.propTypes = {
+  contacts: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string.isRequired,
+      name: PropTypes.string.isRequired,
+      number: PropTypes.string.isRequired,
+    })
+  ),
+  filter: PropTypes.string,
+};
--- src/components/PhoneBook/AddContact.js
+++ src/components/PhoneBook/AddContact.js
@@ -1,65 +1,72 @@
-import React, { Component } from 'react';
+import { useState } from 'react';
 import PropTypes from 'prop-types';
 import { nanoid } from 'nanoid';
 import { StyledForm, StyledLabel, StyledButton } from './PhoneBook.styled';
 
-export class PhoneBook extends Component {
-  static propTypes = {
-    id: PropTypes.string,
-    name: PropTypes.string,
-    number: PropTypes.string,
-  };
-  state = {
-    id: '',
-    name: '',
-    number: '',
-  };
-  handleChange = event => {
+export const PhoneBook = ({ onSubmit }) => {
+  const [id, setId] = useState('');
+  const [name, setName] = useState('');
+  const [number, setNumber] = useState('');
+
+  const handleChange = event => {
     const { name, value } = event.currentTarget;
-    this.setState({
-      id: nanoid(),
-      [name]: value,
-    });
+    setId(nanoid());
+    switch (name) {
+      case 'name':
+        setName(value);
+        break;
+      case 'number':
+        setNumber(value);
+        break;
+      default:
+        return;
+    }
   };
 
-  handleSubmit = event => {
+  const handleSubmit = event => {
     event.preventDefault();
-    this.props.onSubmit(this.state);
-    this.stateReset();
+    stateReset();
+    return onSubmit({ id, name, number });
   };
 
-  stateReset = () => {
-    this.setState({ id: '', name: '', number: '' });
+  const stateReset = () => {
+    setId('');
+    setName('');
+    setNumber('');
   };
-  render() {
-    return (
-      <StyledForm onSubmit={this.handleSubmit}>
-        <StyledLabel>
-          Name
-          <input
-            type="text"
-            name="name"
-            pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-            title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-            required
-            value={this.state.name}
-            onChange={this.handleChange}
-          />
-        </StyledLabel>
-        <StyledLabel>
-          Number
-          <input
-            type="tel"
-            name="number"
-            pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
-            title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
-            required
-            value={this.state.number}
-            onChange={this.handleChange}
-          />
-        </StyledLabel>
-        <StyledButton type="submit">Add contact</StyledButton>
-      </StyledForm>
-    );
-  }
-}
+
+  return (
+    <StyledForm onSubmit={handleSubmit}>
+      <StyledLabel>
+        Name
+        <input
+          type="text"
+          name="name"
+          pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+          title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+          required
+          value={name}
+          onChange={handleChange}
+        />
+      </StyledLabel>
+      <StyledLabel>
+        Number
+        <input
+          type="tel"
+          name="number"
+          pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+          title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+          required
+          value={number}
+          onChange={handleChange}
+        />
+      </StyledLabel>
+      <StyledButton type="submit">Add contact</StyledButton>
+    </StyledForm>
+  );
+};
+PhoneBook.propTypes = {
+  id: PropTypes.string,
+  name: PropTypes.string,
+  number: PropTypes.string,
+};
