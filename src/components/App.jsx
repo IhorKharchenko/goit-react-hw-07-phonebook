2ed58b539dbd3ac9a643fbb4acac180f20133fc5
Ihor Kharchenko <ihor.stavr@gmail.com>
2023-02-12 13:05:58 +0200

    push for checking deployment

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,18 +6,6 @@ import { Box } from 'components/Box';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
-// const useLocalStorage = (key, defaultValue) => {
-//   const [state, setState] = useState(() => {
-//     return JSON.parse(window.localStorage.getItem(key)) ?? defaultValue;
-//   });
-
-//   useEffect(() => {
-//     window.localStorage.setItem(key, JSON.stringify(state));
-//   }, [key, state]);
-
-//   return [state, setState];
-// };
-
 export const App = () => {
   return (
     <Box as="main" p="4" width="620px" ml="auto" mr="auto" bg="secondaryText">
